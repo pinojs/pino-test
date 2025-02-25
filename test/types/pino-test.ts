2039e786b3d8c5fa4f268214cb1ccdf6c886c379
Simone Sanfratello <simone.sanfra@gmail.com>
2025-02-25 17:46:16 +0100

    chore: update deps, lint config (#21)

--- test/types/pino-test.ts
+++ test/types/pino-test.ts
@@ -1,5 +1,7 @@
-import pino from 'pino';
-import pinoTest from '../../pino-test';
+/* eslint-disable */
+
+import pino from 'pino'
+import pinoTest from '../../pino-test'
 
 const stream = pinoTest.sink()
 const logger = pino(stream)
@@ -8,17 +10,17 @@ logger.info('hello world')
 logger.info('by world')
 
 async function onceType() {
-    const expected = { msg: 'hello world', level: 30 }
-    await pinoTest.once(stream, expected)
-    await pinoTest.once(stream, (received: any) => received.msg === 'hello world')
+  const expected = { msg: 'hello world', level: 30 }
+  await pinoTest.once(stream, expected)
+  await pinoTest.once(stream, (received: any) => received.msg === 'hello world')
 }
 
 async function consecutiveType() {
-    const expected = [
-        { msg: 'hello world', level: 30 },
-        (received: any) => received.msg === 'hi world'
-    ]
-    await pinoTest.consecutive(stream, expected)
+  const expected = [
+    { msg: 'hello world', level: 30 },
+    (received: any) => received.msg === 'hi world'
+  ]
+  await pinoTest.consecutive(stream, expected)
 }
 
 const stream1 = pinoTest.sink({ emitErrorEvent: true })
